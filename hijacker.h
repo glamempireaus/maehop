0eadc736ff4c2d77b635e4bed8805963d7297f58
glamempireaus <8907434+glamempireaus@users.noreply.github.com>
2023-08-26 13:29:24 +1000

    Update hijacker.h

--- hijacker.h
+++ hijacker.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #define SeDebugPriv 20
 #define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
 #define STATUS_INFO_LENGTH_MISMATCH ((NTSTATUS)0xC0000004)
@@ -5,52 +7,45 @@
 #define ProcessHandleType 0x7
 #define SystemHandleInformation 16 
 
-/*
-STRUCTURES NEEDED FOR NTOPENPROCESS:
-*/
-typedef struct _UNICODE_STRING {
+namespace Hijacker {
+
+// Structures necessary for NtOpenProcess.
+struct UNICODE_STRING {
     USHORT Length;
     USHORT MaximumLength;
     PWCH   Buffer;
-} UNICODE_STRING, * PUNICODE_STRING;
+};
 
-typedef struct _OBJECT_ATTRIBUTES {
+struct OBJECT_ATTRIBUTES {
     ULONG           Length;
     HANDLE          RootDirectory;
-    PUNICODE_STRING ObjectName;
+    UNICODE_STRING* ObjectName;
     ULONG           Attributes;
     PVOID           SecurityDescriptor;
     PVOID           SecurityQualityOfService;
-}  OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+};
 
-typedef struct _CLIENT_ID
-{
+struct CLIENT_ID {
     PVOID UniqueProcess;
     PVOID UniqueThread;
-} CLIENT_ID, * PCLIENT_ID;
+};
 
-/*
-STRUCTURES NEEDED FOR HANDLE INFORMATION:
-*/
-typedef struct _SYSTEM_HANDLE_TABLE_ENTRY_INFO
-{
+// Structures necessary for handle information.
+struct SYSTEM_HANDLE {
     ULONG ProcessId;
     BYTE ObjectTypeNumber;
     BYTE Flags;
     USHORT Handle;
     PVOID Object;
     ACCESS_MASK GrantedAccess;
-} SYSTEM_HANDLE, * PSYSTEM_HANDLE;
+};
 
-typedef struct _SYSTEM_HANDLE_INFORMATION
-{
+struct SYSTEM_HANDLE_INFORMATION {
     ULONG HandleCount; 
     SYSTEM_HANDLE Handles[1];
-} SYSTEM_HANDLE_INFORMATION, * PSYSTEM_HANDLE_INFORMATION;
+};
 
-/*
-FUNCTION PROTOTYPES:
-*/
+// Function prototypes for internal Windows functions.
 typedef NTSTATUS(NTAPI* _NtDuplicateObject)(
     HANDLE SourceProcessHandle,
     HANDLE SourceHandle,
@@ -71,8 +66,8 @@ typedef NTSTATUS(NTAPI* _RtlAdjustPrivilege)(
 typedef NTSYSAPI NTSTATUS(NTAPI* _NtOpenProcess)(
     PHANDLE            ProcessHandle,
     ACCESS_MASK        DesiredAccess,
-    POBJECT_ATTRIBUTES ObjectAttributes,
-    PCLIENT_ID         ClientId
+    OBJECT_ATTRIBUTES* ObjectAttributes,
+    CLIENT_ID*         ClientId
     );
 
 typedef NTSTATUS(NTAPI* _NtQuerySystemInformation)(
@@ -83,3 +78,5 @@ typedef NTSTATUS(NTAPI* _NtQuerySystemInformation)(
     );
 
 HANDLE HijackExistingHandle(DWORD dwTargetProcessId);
+
+} // namespace Hijacker
